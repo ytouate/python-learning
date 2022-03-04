82e4f65338d4a9704374c922960d47d5a787a975
Youssef Touate <ytouate@e1r10p8.1337.ma>
2022-03-04 13:54:44 +0100

    reading from stdin ex03

--- ex03.py
+++ ex03.py
@@ -1,5 +1,5 @@
 from string import punctuation
-
+import sys
 
 def count_spaces(text) -> int:
     spaces = 0
@@ -10,21 +10,30 @@ def count_spaces(text) -> int:
 
 
 def text_analyzer(*args):
-    if len(args) != 1:
-        print("ERROR\n")
-    uppercase = 0
-    lowercase = 0
-    punc = 0
-    spaces = 0
-    for j in args:
-        for i in j:
-            if i.isupper():
-                uppercase += 1
-            elif i.islower():
-                lowercase += 1
-            elif i in punctuation:
-                punc += 1
-    data = f" The text contains {len(args[0])}\n - {uppercase} upper letters\n - {lowercase} lower letters\n " \
-           f"- {punc} punctuation marks " \
-           f"\n - {count_spaces(args[0])} spaces "
-    return data
+	if len(args) != 1:
+		print("ERROR\n")
+		exit()
+	uppercase = 0
+	lowercase = 0
+	punc = 0
+	spaces = 0
+	for i in args[0]:
+		if i.isupper():
+			uppercase += 1
+		elif i.islower():
+			lowercase += 1
+		elif i in punctuation:
+			punc += 1
+	data = f" The text contains {len(args[0])}\n - {uppercase} upper letters\n - {lowercase} lower letters\n " \
+		   f"- {punc} punctuation marks " \
+		   f"\n - {count_spaces(args[0])} spaces "
+	return data
+
+result = []
+for line in sys.stdin:
+	if "Done" == line.rstrip():
+		break
+	result.append(line)
+
+res = ' '.join(result)
+print(text_analyzer(res))
